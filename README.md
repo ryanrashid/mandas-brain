b6eac00e3b7d6f3765b87ab29a6ce9bed6c667ee
ryanrashid <ryan.rshd@gmail.com>
2020-10-19 05:28:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mandas-prefrontal-cortex
+A simple web app to help Amanda make basic decisions
---
+++ js/script.js
@@ -0,0 +1,7 @@
+function displayOptions(num) {
+  document.getElementById("opt-select").style.display = "none";
+  document.getElementById("options").style.display = "block";
+  for (var i = 3; i <= num; i++) {
+    document.getElementById("opt" + i).style.display = "block";
+  }
+}
