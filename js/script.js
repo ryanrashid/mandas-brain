069781019d5123d4fd2a1208530a279098dbed57
ryanrashid <ryan.rshd@gmail.com>
2020-10-19 15:33:12 -0500

    Style changes and options show up.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 function displayOptions(num) {
   document.getElementById("opt-select").style.display = "none";
   document.getElementById("options").style.display = "block";
-  for (var i = 3; i <= num; i++) {
-    document.getElementById("opt" + i).style.display = "block";
+  var size;
+  if (num > 4) {
+    size = "auto";
+  } else if (num <= 2) {
+    size = "210px";
+  } else {
+    size = "150px";
+  }
+  for (var i = 1; i <= num; i++) {
+    document.getElementById("opt" + i).style.display = "inline-block";
+    document.getElementById("opt" + i).style.height = size;
   }
 }
 
